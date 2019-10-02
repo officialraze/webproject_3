883a968743b34d9b555c12fd607714158e159a63
Raze <33286619+officialraze@users.noreply.github.com>
2019-10-02 16:43:22 +0200

    english language
    - created language file for english

---
+++ language/en.php
@@ -0,0 +1,61 @@
+<?php
+/*
+// --------------------------
+// Webprojekt 3.0
+// Copyright Melvin Lauber
+// --------------------------
+*/
+
+// navigation profile
+define('DISCOVER', 'Discover');
+define('MY_SONGS', 'My songs');
+define('ARTISTS', 'Artists');
+define('EVENTS', 'Events');
+define('SETTINGS', 'Settings');
+define('MY_PLAYLISTS', 'My playlists');
+
+// navigation main content
+define('HOME', 'Home');
+define('GENRES', 'Genres');
+define('PLAYLISTS', 'Playlists');
+define('RADIO', 'Radio');
+
+// content home
+define('RECENTLY_LISTENED', 'Recently listened');
+define('ARTISTS_U_MIGHT_LIKE', 'Artists you might like');
+define('NEW_SONGS', 'New songs');
+define('TODAY', 'Today');
+define('WEEK', 'Week');
+define('MONTH', 'Month');
+
+// login
+define('PLEASE_LOGIN', 'Please login');
+define('NEW_REGISTER', 'New register');
+define('LOGIN', 'Login');
+define('REGISTER', 'Register');
+define('USERNAME', 'Username');
+define('PASSWORD', 'Password');
+define('RETYPE_PASSWORD', 'Retype password');
+define('MAIL', 'E-Mail');
+define('USERNAME_MAIL', 'Username or E-Mail');
+define('LASTNAME', 'Lastname');
+define('FIRSTNAME', 'Firstname');
+
+// artist
+define('IS_FOLLOW', 'Following');
+define('FOLLOW', 'Follow');
+define('POPULAR', 'Popular');
+define('ALBUM', 'Album');
+define('SONGS', 'Songs');
+define('SHOW_ALL', 'Show all');
+define('ADD_NEW_ALBUM', 'Add a new album');
+define('MANAGE_SONGS_ABLUMS', 'Manage songs and albums');
+
+// album overview
+define('OVERVIEW', 'Overview');
+
+// radio
+define('RADIO_AND_LIVESTREAMS', 'Radio und Livestreams');
+
+// misc
+define('BACK', 'Back');
