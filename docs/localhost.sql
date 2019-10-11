f4a92938a274fd1bbddcdf8dac860d0f1b2716b5
Raze <33286619+officialraze@users.noreply.github.com>
2019-10-12 01:56:06 +0200

    update docs
    - db improved

--- docs/localhost.sql
+++ docs/localhost.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Erstellungszeit: 25. Sep 2019 um 16:23
--- Server-Version: 10.1.38-MariaDB
--- PHP-Version: 7.3.2
+-- Host: localhost:3306
+-- Erstellungszeit: 11. Okt 2019 um 23:55
+-- Server-Version: 5.6.34-log
+-- PHP-Version: 7.1.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,21 +31,21 @@ USE `db_music_player`;
 -- Tabellenstruktur für Tabelle `album`
 --
 
-CREATE TABLE IF NOT EXISTS `album` (
-  `artist_id` int(11) NOT NULL,
-  `album_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `album` (
+  `album_id` int(11) NOT NULL,
   `album_name` varchar(255) NOT NULL,
   `album_year` int(4) NOT NULL,
-  PRIMARY KEY (`album_id`),
-  KEY `artist_id` (`artist_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `artist_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Daten für Tabelle `album`
 --
 
-INSERT INTO `album` (`artist_id`, `album_id`, `album_name`, `album_year`) VALUES
-(1, 1, 'Lost - Single', 2019);
+INSERT INTO `album` (`album_id`, `album_name`, `album_year`, `artist_id`) VALUES
+(1, 'Lost - Single', 2019, 1),
+(2, 'Digital World', 2018, 1),
+(3, 'We Could Be - Single', 2019, 1);
 
 -- --------------------------------------------------------
 
@@ -53,22 +53,20 @@ INSERT INTO `album` (`artist_id`, `album_id`, `album_name`, `album_year`) VALUES
 -- Tabellenstruktur für Tabelle `artist`
 --
 
-CREATE TABLE IF NOT EXISTS `artist` (
-  `artist_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `artist` (
+  `artist_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `artist_firstname` varchar(255) NOT NULL,
   `artist_lastname` varchar(255) NOT NULL,
-  `biography` text NOT NULL,
-  PRIMARY KEY (`artist_id`),
-  KEY `artist_fk0` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `biography` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Daten für Tabelle `artist`
 --
 
 INSERT INTO `artist` (`artist_id`, `user_id`, `artist_firstname`, `artist_lastname`, `biography`) VALUES
-(1, 1, 'raze.exe', '', 'raze.exe is a 19 years old swiss producer.');
+(1, 1, 'raze.exe', '', 'raze.exe is a 19 year old swiss producer who started making music in 2012. Inspired from artists like Virtual Riot or Skrillex, he\'s now making similar EDM music. His main genre is Future Bass but is also known for his few Dubstep and Glitch Hop songs. His song \'Ocean\' starts to get known.');
 
 -- --------------------------------------------------------
 
@@ -76,11 +74,9 @@ INSERT INTO `artist` (`artist_id`, `user_id`, `artist_firstname`, `artist_lastna
 -- Tabellenstruktur für Tabelle `following_artist`
 --
 
-CREATE TABLE IF NOT EXISTS `following_artist` (
+CREATE TABLE `following_artist` (
   `user_id_link` int(11) NOT NULL,
-  `artist_id` int(11) NOT NULL,
-  KEY `following_artist_fk0` (`user_id_link`),
-  KEY `following_artist_fk1` (`artist_id`)
+  `artist_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -89,11 +85,10 @@ CREATE TABLE IF NOT EXISTS `following_artist` (
 -- Tabellenstruktur für Tabelle `genre`
 --
 
-CREATE TABLE IF NOT EXISTS `genre` (
-  `genre_id` int(11) NOT NULL AUTO_INCREMENT,
-  `genre_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`genre_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+CREATE TABLE `genre` (
+  `genre_id` int(11) NOT NULL,
+  `genre_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Daten für Tabelle `genre`
@@ -108,11 +103,10 @@ INSERT INTO `genre` (`genre_id`, `genre_name`) VALUES
 -- Tabellenstruktur für Tabelle `livestream`
 --
 
-CREATE TABLE IF NOT EXISTS `livestream` (
-  `livestream_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `livestream` (
+  `livestream_id` int(11) NOT NULL,
   `livestream_name` varchar(255) NOT NULL,
-  `livestream_url` text NOT NULL,
-  PRIMARY KEY (`livestream_id`)
+  `livestream_url` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -121,21 +115,20 @@ CREATE TABLE IF NOT EXISTS `livestream` (
 -- Tabellenstruktur für Tabelle `playlist`
 --
 
-CREATE TABLE IF NOT EXISTS `playlist` (
-  `playlist_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `playlist` (
+  `playlist_id` int(11) NOT NULL,
   `playlist_name` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `playlist_description` text NOT NULL,
-  PRIMARY KEY (`playlist_id`),
-  KEY `playlist_fk0` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `playlist_description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Daten für Tabelle `playlist`
 --
 
 INSERT INTO `playlist` (`playlist_id`, `playlist_name`, `user_id`, `playlist_description`) VALUES
-(1, 'molvin', 1, 'songs of molvin');
+(1, 'newest of raze', 1, 'Always listen to the newest releases of raze.exe'),
+(2, 'Top 50 EDM Charts', 1, 'Listen to the top 50 of EDM tracks.');
 
 -- --------------------------------------------------------
 
@@ -143,11 +136,9 @@ INSERT INTO `playlist` (`playlist_id`, `playlist_name`, `user_id`, `playlist_des
 -- Tabellenstruktur für Tabelle `playlist_song`
 --
 
-CREATE TABLE IF NOT EXISTS `playlist_song` (
+CREATE TABLE `playlist_song` (
   `playlist_id` int(11) NOT NULL,
-  `song_id` int(11) NOT NULL,
-  KEY `playlist_song_fk0` (`playlist_id`),
-  KEY `playlist_song_fk1` (`song_id`)
+  `song_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -155,6 +146,7 @@ CREATE TABLE IF NOT EXISTS `playlist_song` (
 --
 
 INSERT INTO `playlist_song` (`playlist_id`, `song_id`) VALUES
+(1, 1),
 (1, 2);
 
 -- --------------------------------------------------------
@@ -163,11 +155,9 @@ INSERT INTO `playlist_song` (`playlist_id`, `song_id`) VALUES
 -- Tabellenstruktur für Tabelle `saved_songs`
 --
 
-CREATE TABLE IF NOT EXISTS `saved_songs` (
+CREATE TABLE `saved_songs` (
   `user_id_link` int(11) NOT NULL,
-  `song_id` int(11) NOT NULL,
-  KEY `saved_songs_fk0` (`user_id_link`),
-  KEY `saved_songs_fk1` (`song_id`)
+  `song_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -175,6 +165,7 @@ CREATE TABLE IF NOT EXISTS `saved_songs` (
 --
 
 INSERT INTO `saved_songs` (`user_id_link`, `song_id`) VALUES
+(1, 1),
 (1, 2);
 
 -- --------------------------------------------------------
@@ -183,25 +174,27 @@ INSERT INTO `saved_songs` (`user_id_link`, `song_id`) VALUES
 -- Tabellenstruktur für Tabelle `song`
 --
 
-CREATE TABLE IF NOT EXISTS `song` (
-  `song_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `song` (
+  `song_id` int(11) NOT NULL,
   `artist_id` int(11) NOT NULL,
   `song_name` varchar(255) NOT NULL,
   `album_id` int(11) NOT NULL,
   `length` varchar(255) NOT NULL,
-  `genre_id` int(11) NOT NULL,
-  PRIMARY KEY (`song_id`),
-  KEY `song_fk0` (`artist_id`),
-  KEY `song_fk1` (`album_id`),
-  KEY `song_fk2` (`genre_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `genre_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Daten für Tabelle `song`
 --
 
 INSERT INTO `song` (`song_id`, `artist_id`, `song_name`, `album_id`, `length`, `genre_id`) VALUES
-(2, 1, 'Lost', 1, '03:17', 1);
+(1, 1, 'Lost', 1, '03:18', 1),
+(2, 1, 'Ocean', 2, '02:14', 1),
+(5, 1, 'Ambient', 2, '02:37', 1),
+(6, 1, 'Bells', 2, '02:18', 1),
+(7, 1, 'We Could Be', 3, '02:07', 1),
+(8, 1, 'Claws', 2, '02:48', 1),
+(9, 1, 'Goodbye', 2, '01:50', 1);
 
 -- --------------------------------------------------------
 
@@ -209,8 +202,8 @@ INSERT INTO `song` (`song_id`, `artist_id`, `song_name`, `album_id`, `length`, `
 -- Tabellenstruktur für Tabelle `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `firstname` varchar(255) NOT NULL,
@@ -218,27 +211,132 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password_hash` varchar(255) NOT NULL,
   `password_token` varchar(255) NOT NULL,
   `is_artist` int(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `has_darkmode` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Daten für Tabelle `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `firstname`, `lastname`, `password_hash`, `password_token`, `is_artist`) VALUES
-(1, 'melvin.lauber', 'molvinlauber@gmail.com', 'Melvin', 'Lauber', '$2y$10$Mw4tBnzqEH4ME57esdBb.OBcVFQGZHY/RC/dQPz5Lrg.jGa3tDjUS', '1234', 1);
+INSERT INTO `users` (`id`, `username`, `email`, `firstname`, `lastname`, `password_hash`, `password_token`, `is_artist`, `has_darkmode`) VALUES
+(1, 'molvin95', 'molvinlauber@gmail.com', 'Melvin', 'Lauber', '$2y$10$kTzuINN0JEYWwZCH2cJfHO4nbtXuYyp0VGROPtVMlgX/U/W1O0dpa', '1234', 1, 1);
 
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`album_id`);
+
+--
+-- Indizes für die Tabelle `artist`
+--
+ALTER TABLE `artist`
+  ADD PRIMARY KEY (`artist_id`),
+  ADD KEY `artist_fk0` (`user_id`);
+
+--
+-- Indizes für die Tabelle `following_artist`
+--
+ALTER TABLE `following_artist`
+  ADD KEY `following_artist_fk0` (`user_id_link`),
+  ADD KEY `following_artist_fk1` (`artist_id`);
+
+--
+-- Indizes für die Tabelle `genre`
+--
+ALTER TABLE `genre`
+  ADD PRIMARY KEY (`genre_id`);
+
+--
+-- Indizes für die Tabelle `livestream`
+--
+ALTER TABLE `livestream`
+  ADD PRIMARY KEY (`livestream_id`);
+
+--
+-- Indizes für die Tabelle `playlist`
+--
+ALTER TABLE `playlist`
+  ADD PRIMARY KEY (`playlist_id`),
+  ADD KEY `playlist_fk0` (`user_id`);
+
+--
+-- Indizes für die Tabelle `playlist_song`
+--
+ALTER TABLE `playlist_song`
+  ADD KEY `playlist_song_fk0` (`playlist_id`),
+  ADD KEY `playlist_song_fk1` (`song_id`);
+
+--
+-- Indizes für die Tabelle `saved_songs`
+--
+ALTER TABLE `saved_songs`
+  ADD KEY `saved_songs_fk0` (`user_id_link`),
+  ADD KEY `saved_songs_fk1` (`song_id`);
+
+--
+-- Indizes für die Tabelle `song`
+--
+ALTER TABLE `song`
+  ADD PRIMARY KEY (`song_id`),
+  ADD KEY `song_fk0` (`artist_id`),
+  ADD KEY `song_fk1` (`album_id`),
+  ADD KEY `song_fk2` (`genre_id`);
+
+--
+-- Indizes für die Tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `album`
+--
+ALTER TABLE `album`
+  MODIFY `album_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT für Tabelle `artist`
+--
+ALTER TABLE `artist`
+  MODIFY `artist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT für Tabelle `genre`
+--
+ALTER TABLE `genre`
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT für Tabelle `livestream`
+--
+ALTER TABLE `livestream`
+  MODIFY `livestream_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT für Tabelle `playlist`
+--
+ALTER TABLE `playlist`
+  MODIFY `playlist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT für Tabelle `song`
+--
+ALTER TABLE `song`
+  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT für Tabelle `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints der exportierten Tabellen
 --
-
---
--- Constraints der Tabelle `album`
---
-ALTER TABLE `album`
-  ADD CONSTRAINT `album_ibfk_1` FOREIGN KEY (`artist_id`) REFERENCES `artist` (`artist_id`);
 
 --
 -- Constraints der Tabelle `artist`
